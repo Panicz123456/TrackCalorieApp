5d4f813e9ad6ab061bdd6cfc5a1f2e9af914b619
Patryk Sierawski <105878505+Panicz123456@users.noreply.github.com>
2024-04-24 16:07:58 +0200

    update

--- tracalorie-app/js/app.js
+++ tracalorie-app/js/app.js
@@ -13,6 +13,34 @@ class CalorieTracker {
 
   addWorkout(workout) {
     this._workouts.push(workout);
-    this._totalCalories += workout.calories;
+    this._totalCalories -= workout.calories;
   }
 }
+
+class Meal {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+
+class Workout {
+  constructor(name, calories) {
+    this.id = Math.random().toString(16).slice(2);
+    this.name = name;
+    this.calories = calories;
+  }
+}
+
+const tracker = new CalorieTracker();
+
+const breakfast = new Meal("Breakfast", 400);
+tracker.addMeal(breakfast);
+
+const run = new Workout("Morning Run", 300);
+tracker.addWorkout(run);
+
+console.log(tracker._meals);
+console.log(tracker._workouts);
+console.log(tracker._totalCalories);
